66e000d52893ca27790eb3282109fa20be0ea8f4
予 <601010302@qq.com>
2022-12-15 14:27:34 +0800

    useMemo完整

--- src/view/useMemo/index.tsx
+++ src/view/useMemo/index.tsx
@@ -1,22 +1,27 @@
 import React, { useState, useMemo } from 'react'
 
-const results = (a: number, b: number) => {
-  console.log(1);
-  return a + b;
-}
 
 export default function Index() {
   const [count, setCount] = useState(1)
-
+  let a = 10
+  let b = 10
+  if (count % 10 === 0) {
+    a = 100
+  }
+  const results = (a: number, b: number) => {
+    console.log(1);
+    return a + b;
+  }
   //原本在调用results函数的时候只调用的了一次，但是在改变count状态的时候页面重新渲染，再次触发了results函数，这样性能就不好了
   //useMemo是用来缓存函数返回的结果，与usecallback相似
   const result = useMemo(() => {
-    return results(10, 10)
-  }, [])
+    return results(a, b)
+  }, [a, b]) //数组是填写依赖项的，当调用函数的依赖发生了改变要重新调用函数,当a或者b的值发生了改变重新调用函数
   return (
     <div>
       <h1>useMemo钩子</h1>
-      <h3>{count}</h3><h3>{result}</h3>
+      <h3>{count}</h3>
+      <h3>{result}</h3>
       <button onClick={() => setCount(count => count + 1)}>点击count递增</button>
     </div>
   )
